3f1891026c06d960eeb12884d72ead2cbd4de288
서지유 <jihoseo2008@gmail.com>
2026-02-10 11:50:30 +0900

    feat: 피드백 페이지 path 변경

--- apps/mentor/src/app/router.tsx
+++ apps/mentor/src/app/router.tsx
@@ -32,7 +32,7 @@ export const router = createBrowserRouter(
                             element: <ManagePage />,
                         },
                         {
-                            path: '/feedback',
+                            path: '//mentoring/:menteeId/feedback',
                             element: <FeedbackPage />,
                         },
                     ],
