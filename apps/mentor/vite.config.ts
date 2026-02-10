9e250aae31d62cc81f9c057e47c6729ead82e587
서지유 <jihoseo2008@gmail.com>
2026-02-10 19:06:02 +0900

    fix: 괄호 수정

--- apps/mentor/vite.config.ts
+++ apps/mentor/vite.config.ts
@@ -4,69 +4,70 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({
-    base: '/mentor/',
-    envDir: './',
-    plugins: [
-        tailwindcss(),
-        react({
-            babel: {
-                plugins: [['babel-plugin-react-compiler']],
-            },
-        }),
-        VitePWA({
-            registerType: 'autoUpdate',
-            filename: 'sw.js',
-            injectRegister: 'auto',
-            manifest: {
-                id: '/mentor/',
-                name: 'Mentor App',
-                short_name: 'Mentor',
-                start_url: '/mentor/',
-                scope: '/mentor/',
-                display: 'standalone',
-                background_color: '#001871',
-                theme_color: '#001871',
-                icons: [
-                    {
-                        src: '/mentor/icons/icon-192.png',
-                        sizes: '192x192',
-                        type: 'image/png',
-                        purpose: 'maskable',
-                    },
-                    {
-                        src: '/mentor/icons/icon-512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable',
-                    },
-                ],
-            },
-            devOptions: {
-                enabled: true,
-            },
-        }),
-    ],
+export default defineConfig(
+    {
+        base: '/mentor/',
+        envDir: './',
+        plugins: [
+            tailwindcss(),
+            react({
+                babel: {
+                    plugins: [['babel-plugin-react-compiler']],
+                },
+            }),
+            VitePWA({
+                registerType: 'autoUpdate',
+                filename: 'sw.js',
+                injectRegister: 'auto',
+                manifest: {
+                    id: '/mentor/',
+                    name: 'Mentor App',
+                    short_name: 'Mentor',
+                    start_url: '/mentor/',
+                    scope: '/mentor/',
+                    display: 'standalone',
+                    background_color: '#001871',
+                    theme_color: '#001871',
+                    icons: [
+                        {
+                            src: '/mentor/icons/icon-192.png',
+                            sizes: '192x192',
+                            type: 'image/png',
+                            purpose: 'maskable',
+                        },
+                        {
+                            src: '/mentor/icons/icon-512.png',
+                            sizes: '512x512',
+                            type: 'image/png',
+                            purpose: 'maskable',
+                        },
+                    ],
+                },
+                devOptions: {
+                    enabled: true,
+                },
+            }),
+        ],
 
-    resolve: {
-        alias: {
-            '@package': path.resolve(__dirname, '../../package'),
-            '@': path.resolve(__dirname, 'src'),
-            '@app': path.resolve(__dirname, 'src/app'),
-            '@assets': path.resolve(__dirname, 'src/assets'),
-            '@entities': path.resolve(__dirname, 'src/entities'),
-            '@features': path.resolve(__dirname, 'src/features'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@shared': path.resolve(__dirname, 'src/shared'),
+        resolve: {
+            alias: {
+                '@package': path.resolve(__dirname, '../../package'),
+                '@': path.resolve(__dirname, 'src'),
+                '@app': path.resolve(__dirname, 'src/app'),
+                '@assets': path.resolve(__dirname, 'src/assets'),
+                '@entities': path.resolve(__dirname, 'src/entities'),
+                '@features': path.resolve(__dirname, 'src/features'),
+                '@pages': path.resolve(__dirname, 'src/pages'),
+                '@shared': path.resolve(__dirname, 'src/shared'),
+            },
         },
-    },
-  },
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://221.148.101.200:8089',
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
-})
+    }
+    // server: {
+    //   proxy: {
+    //     '/api': {
+    //       target: 'http://221.148.101.200:8089',
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
+);
